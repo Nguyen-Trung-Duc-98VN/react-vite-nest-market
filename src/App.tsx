cf016e91ffe92f80eec8399bde443d7e20864955
NguyenTrungDuc98 <ducnt@hasaki.vn>
2022-06-30 23:36:18 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Images from "~/assets/images/images";
+import HomePage from "~/pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <HomePage />
+        <Images />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/images/images.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export default function Images({}: Props) {
+  return <div>Images</div>;
+}
